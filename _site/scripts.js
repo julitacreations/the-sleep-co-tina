40a5d590cc1d2a10607076e7a84b21dd16368531
julitacreations <90911265+julitacreations@users.noreply.github.com>
2021-11-04 11:44:04 +0200

    animations added

--- _site/scripts.js
+++ _site/scripts.js
@@ -7,5 +7,9 @@ $( document ).ready(function() {
         $(".mobile-menu").fadeToggle();
     });
 
+    // Animate on Scroll - activate
+    AOS.init({
+        duration: 500 // values from 0 to 3000, with step 50ms
+    });
 
 });
